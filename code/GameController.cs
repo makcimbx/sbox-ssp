7790e9788b62ea0d9863441643ab8545ed58f425
Makcim Ivanov <makcimbx@gmail.com>
2021-08-05 19:41:36 +0300

    Addon Initial Commit

---
+++ code/GameController.cs
@@ -0,0 +1,39 @@
+using Sandbox;
+
+namespace TLC.SSP
+{
+	[Library( "ssp", Title = "Sausage Space Program" )]
+	public partial class GameController : Game
+	{
+		public GameController()
+		{
+
+		}
+
+		public override void ClientJoined( Client cl )
+		{
+			base.ClientJoined( cl );
+			var player = new GroundPlayer();
+			player.Respawn();
+
+			cl.Pawn = player;
+		}
+
+		public override void DoPlayerNoclip( Client player )
+		{
+			if ( player.Pawn is Player basePlayer )
+			{
+				if ( basePlayer.DevController is NoclipController )
+				{
+					Log.Info( "Noclip Mode Off" );
+					basePlayer.DevController = null;
+				}
+				else
+				{
+					Log.Info( "Noclip Mode On" );
+					basePlayer.DevController = new NoclipController();
+				}
+			}
+		}
+	}
+}
---
+++ code/Player.cs
@@ -0,0 +1,30 @@
+﻿using Sandbox;
+
+namespace TLC.SSP 
+{
+	partial class GroundPlayer : Player
+	{
+
+		public override void Respawn()
+		{
+			SetModel( "models/citizen/citizen.vmdl" );
+
+			// Use WalkController for movement (you can make your own PlayerController for 100% control)
+			Controller = new WalkController();
+
+			// Use StandardPlayerAnimator  (you can make your own PlayerAnimator for 100% control)
+			Animator = new StandardPlayerAnimator();
+
+			// Use ThirdPersonCamera (you can make your own Camera for 100% control)
+			Camera = new ThirdPersonCamera();
+
+			EnableAllCollisions = true;
+			EnableDrawing = true;
+			EnableHideInFirstPerson = true;
+			EnableShadowInFirstPerson = true;
+
+			base.Respawn();
+		}
+	}
+
+}
